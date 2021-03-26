7e23f9f7ceb661762e848b58c27519c7a29c939a
GrandMatthew <75782067+GrandMatthew@users.noreply.github.com>
2021-03-26 15:37:32 +0300

    Add some models

---
+++ data/goods.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Goods(SqlAlchemyBase):
+    __tablename__ = 'goods'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    class_name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    weight = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    size = sqlalchemy.Column(sqlalchemy.Integer, default=0)
