13a280992d20f0e85959c76078100ebcc073ebeb
Dmitry <dimalyamin2004@mail.ru>
2021-03-26 14:36:07 +0300

    try to do a 'about_us' page

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, redirect, request, abort, url_for
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key'
 
 
 @app.route('/')
+@app.route('/about_us')
 def index():
-    return render_template('base.html', title='Shop on the coach')
+    return render_template('about_us.html', title='Shop on the coach')
 
 
 def main():
-    app.run()
+    app.run(port=8080)
 
 
 if __name__ == '__main__':
