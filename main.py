c71cd19b3075587c24d6f04bb730469dda294e26
Dmitry <dimalyamin2004@mail.ru>
2021-04-21 21:43:58 +0300

    update main

--- main.py
+++ main.py
@@ -324,7 +324,7 @@ def make_order(customer_id, goods_id):
 def main():
     global_init("db/trading_area.db")
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
 
 
 if __name__ == '__main__':
